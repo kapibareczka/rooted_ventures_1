42a20609e9c4b232f240e7961d7fc4406ae5e4c0
MagoroGraphics <magoro.graphics@gmail.com>
2023-03-23 14:08:07 +0000

    Show blogposts from database in blog.html

--- rooted_ventures/blog/views.py
+++ rooted_ventures/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/blog.html', context)
